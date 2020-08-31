ff6cb3f8d115981c8407a8d10bf3a009e52881cc
timscotttallent <timscotttallent@gmail.com>
2020-08-31 00:49:30 -0400

    push

---
+++ .ipynb_checkpoints/scrape_mars-checkpoint.py
@@ -0,0 +1,66 @@
+from splinter import Browser
+import pandas as pd
+import pymongo
+from flask import Flask
+from bs4 import BeautifulSoup
+import requests
+
+def scrape():
+    executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    news_title, news_p = scrape_news()
+
+    mars_dict= {}
+
+    mars_dict['title'] = news_title
+    mars_dict['paragraph'] = news_p
+
+    mars_dict['main_image'] = scrape_mars_image()
+
+    mars_dict['mars_facts'] = mars_facts()
+
+    mars_dict['mars_hemispheres'] = mars_hemisphere()
+
+    browser.quit()
+    return mars_dict 
+
+# Mars News
+def scrape_news():
+
+    #url to be scraped
+    url = 'https://mars.nasa.gov/news/'
+    #retrive page w/ request module
+    response = requests.get(url)
+    browser.visit(url)
+    #create bs object and parse w/ html parser
+    soup = BeautifulSoup(response.text, 'html.parser')
+    #print(soup.prettify())
+    #Scrape the NASA Mars News Site and collect the latest News Title and Paragraph Text
+    try:
+        news_title = soup.find('div', class_= 'content_title').text
+        news_p = soup.find('div', class_= 'rollover_description').text
+        #print(news_title)
+        #print(news_p)
+    except AttributeError:
+        return None, None
+    #browser.quit()
+
+    return news_title, news_p
+
+# Mars Image
+def scrape_image():
+        #url to be scraped
+        url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+        #retrive page w/ request module
+        response = requests.get(url)
+        browser.visit(url)
+        #create bs object and parse w/ html parser
+        soup = BeautifulSoup(response.text, 'html.parser')
+        #print(soup.prettify())
+    try:
+        relative_image_path = soup.find_all('img')[2]["src"]
+        carousel_item = url + relative_image_path
+        #print(carousel_item)
+    except AttributeError:
+        return None, None 
